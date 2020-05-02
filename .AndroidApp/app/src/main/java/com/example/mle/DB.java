e0c4163d62af4491acd0d7684dc680becdcf9721
Mihael Lukas <mihael.lukas@novena.hr>
2020-05-02 23:39:31 +0200

    Složeno spajanje na SQL Server, napravljena posebna klasa koja obavlja dohvaćanje podataka u liste (trenutno samo Examples). Složen gumb na početnoj stranici tako da testiram dohvaćanje podataka iz baze. Implementiran stil za prikaz podataka (samo raw prikaz podataka) pomoću RelativeLayounta, zajedno sa ScrollView-om. Promijenjene glavne boje aplikacije (u plavu, ista kao i na webu), te title stavljen u bijelo i stavljena nova MenuBar ikonica.

---
+++ .AndroidApp/app/src/main/java/com/example/mle/DB.java
@@ -0,0 +1,72 @@
+package com.example.mle;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.StrictMode;
+import androidx.annotation.RequiresApi;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DB {
+    static String ip = "192.168.8.109", port = "1433", dbName = "MLE";
+
+    @SuppressLint("NewApi")
+    public static Connection ConnectToDB() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        Connection connection = null;
+        String ConnectionURL = null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";databaseName=" + dbName;
+            connection = DriverManager.getConnection(ConnectionURL, "zv", "zv");
+        } catch (Exception ex) {
+            String e = ex.getMessage();
+        }
+        return connection;
+    }
+
+    public static ResultSet ExecuteQuery(Connection c, String q){
+        try {
+            Statement st = c.createStatement();
+            ResultSet rs = st.executeQuery(q);
+            return rs;
+        }
+        catch (Exception ex){
+            return null;
+        }
+    }
+
+    public static class Example {
+        public int Id;
+        public String Content;
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        public static List<Example> GetAllExamples() {
+            List<Example> examples = new ArrayList<>();
+            String q = "select * from Example";
+            Connection c = ConnectToDB();
+            if (c != null) {
+                ResultSet rs = ExecuteQuery(c, q);
+                if(rs != null) {
+                    try {
+                        while (rs.next()) {
+                            Example e = new Example();
+                            e.Id = Integer.parseInt(rs.getString("Id"));
+                            e.Content = rs.getString("Content");
+                            examples.add(e);
+                        }
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+            return examples;
+        }
+    }
+}
--- .AndroidApp/app/src/main/java/com/example/mle/FirstFragment.java
+++ .AndroidApp/app/src/main/java/com/example/mle/FirstFragment.java
@@ -1,13 +1,21 @@
 package com.example.mle;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import java.util.List;
 
 public class FirstFragment extends Fragment {
 
@@ -16,12 +24,24 @@ public class FirstFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        return view;
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull final View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Button button = (Button) view.findViewById(R.id.btnConnect);
+        button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                TextView tv = (TextView) view.findViewById(R.id.tvText);
+                List<DB.Example> list = DB.Example.GetAllExamples();
+                tv.setText(list.get(1).Content);
+                tv.setMovementMethod(new ScrollingMovementMethod());
+            }
+        });
 
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
